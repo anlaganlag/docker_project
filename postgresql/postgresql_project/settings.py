da23ce468c74f15d1b0a07a76269ffc27b91c976
gal <tanjhu@163.com>
2020-03-03 16:24:31 +0800

    开心,首次docker设置postgresql

--- postgresql/postgresql_project/settings.py
+++ postgresql/postgresql_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r=aiiekc%7yweaywl+jf_v3i+8$6w5e#*($t&q%-ixzea8pl9%'
+SECRET_KEY = '8&^kx6md$zb!hg%8iq4*mw)bacj2j@d9ndu-k*eovx)!!ooxkc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -83,6 +83,14 @@ DATABASES = {
         'PORT': 5432
     }
 }
+
+
+#DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
+#}
 
 
 # Password validation
